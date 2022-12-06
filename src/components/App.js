247bf8c0417c794749dd21abfa7ce71d7724a9ff
Mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-12-06 21:27:13 +0330

    Reafactoring App

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,32 @@
-import React from "react";
 import SearchBar from "./SearchBar";
-import youtube from '../apis/youtube'
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
+import { useEffect, useState } from "react";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("react js");
 
-  componentDidMount() {
-    this.onTermSubmit('react js')
-  }
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
-  onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term
-      }
-    })
-
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    })
-  };
-
-  onVideoSelect = video => {
-    this.setState({ selectedVideo: video })
-  }
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: '' };
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  onInputChange = event => {
-    this.setState({ term: event.target.value });
-  };
-
-  onFormSubmit = event => {
+  const onSubmit = (event) => {
     event.preventDefault();
 
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label style={{ fontSize: '20px' }}>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label style={{ fontSize: "20px" }}>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
